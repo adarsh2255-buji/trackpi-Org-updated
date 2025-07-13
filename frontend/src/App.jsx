4ca82dee801a62d14bcbcfe43ac7d49d45c1dc84
hasna679 <hasnahassan679@gmail.com>
2025-07-13 16:13:38 +0530

    create FAQ section with accordion and styled question list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import StartCourse from './pages/StartCourse'
 import PhoneNUmber from './pages/PhoneNUmber'
+import Faq from './pages/Faq'
 
 // background layout
 import LayoutA from './components/LayoutA'
@@ -11,20 +12,22 @@ import LayoutB from './components/LayoutB'
 import CourseSection from './pages/CourseSection'
 
 
+
 function App() {
   return (
     <>
-    <Routes>
-      <Route element={<LayoutA />}>
-        <Route path='/' element={<Home />} />
-        <Route path='/phone-number' element={<PhoneNUmber />} />
-      </Route>
+      <Routes>
+        <Route element={<LayoutA />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/phone-number' element={<PhoneNUmber />} />
+        </Route>
 
-      <Route element={<LayoutB />}>
-        <Route path='/start-course' element={<StartCourse />} />
-        <Route path='/course-section' element={<CourseSection />} />
-      </Route>
-    </Routes>
+        <Route element={<LayoutB />}>
+          <Route path='/start-course' element={<StartCourse />} />
+          <Route path='/course-section' element={<CourseSection />} />
+          <Route path="/faq" element={<Faq />} />
+        </Route>
+      </Routes>
     </>
   )
 }
