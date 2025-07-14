5f4a322e5ec5e2c13421989fb37720d292883941
adarsh <adarshsomanasrt@gmail.com>
2025-07-14 13:14:48 +0530

    Add HeaderA component to LayoutA for improved layout structure

--- frontend/src/components/LayoutA.jsx
+++ frontend/src/components/LayoutA.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import './css/LayoutA.css'
+import HeaderA from './HeaderA'
 
 const LayoutA = () => {
   return (
+    <>
+    <HeaderA />
     <div className='background-a'>
         <Outlet />
     </div>
+    </>
   )
 }
 
